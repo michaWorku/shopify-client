490076a4450fce8cebfe5f9f6fda98745233221f
michaworku <mikias27worku@gmail.com>
2022-03-20 16:14:31 +0300

    configuring routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Cart, Home, Login, Product, ProductList, Register, Success } from './pages'
+
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/' >
+          <Route index element={<Home/>}></Route>
+          <Route path='products/:category' element={<ProductList />}/>
+          <Route path='product/:id' element={<Product />}/>
+          <Route path='cart' element={<Cart/>}/>
+          <Route path='success' element={<Success/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='register' element={<Register/>}/>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
+
+
+
 
 export default App
