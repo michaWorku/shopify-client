e7706f667f7c5c1ea3bda6dd02eae3995630e60e
michaworku <mikias27worku@gmail.com>
2022-03-20 17:53:29 +0300

    Navbar design

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,6 +1,37 @@
+import { Center, Container, Input, Language, Left, Logo, MenuItem, Right, SearchContainer, Wrapper } from "./styled"
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>
+            EN
+          </Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{color:"gray",fontSize: '16px'}}/>
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>
+            Shopify
+          </Logo>
+        </Center>
+        <Right>
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+            <MenuItem>
+              <Badge badgeContent={4} color="secondary">
+                <ShoppingCartOutlinedIcon/>
+              </Badge>
+            </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
   )
 }
 
