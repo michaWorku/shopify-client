c25aa0a766f0c084ca53ce431456e25679252145
michaworku <mikias27worku@gmail.com>
2022-03-20 19:42:49 +0300

    slider componet ui

--- client/src/components/Slider/index.jsx
+++ client/src/components/Slider/index.jsx
@@ -1,7 +1,44 @@
+import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { Arrow, Button, Container, Desc, Image, ImgContainer, InfoContainer, Slide, Title, Wrapper } from './styled';
+import { sliderItems } from '../../data'
+import { useState } from 'react';
+
 const Slider = () => {
+
+  const [slideIndex, setSlideIndex] = useState(0)
+
+  const handleClick = ( direction)=>{
+    if(direction)
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+    else  
+      setSlideIndex(slideIndex < 2 ? setSlideIndex + 1 : 0)
+  }
+
   return (
-    <div>Slider</div>
-  )
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlinedIcon />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlinedIcon />
+      </Arrow>
+    </Container>
+  );
 }
 
 export default Slider
