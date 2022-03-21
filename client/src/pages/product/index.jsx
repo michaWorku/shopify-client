eb634be0ab7901b48a85e65ded9ecbbdd35be584
michaworku <mikias27worku@gmail.com>
2022-03-21 13:00:19 +0300

    shopping product page design

--- client/src/pages/product/index.jsx
+++ client/src/pages/product/index.jsx
@@ -1,6 +1,55 @@
+import { Announcement, Footer, Navbar, NewsLetter } from "../../components"
+import { Container, Desc, ImageContainer, Image, InfoContainer, Price, Title, Wrapper, FilterContainer, Filter, FilterTitle, FilterColor, FilterSizeOption, FilterSize, AddContainer, AmmountContainer, Button, Ammount } from "./styled"
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+
 const Product = () => {
   return (
-    <div>Product</div>
+    <Container>
+      <Navbar/>
+      <Announcement/>
+        <Wrapper>
+          <ImageContainer>
+            <Image src="https://i.ibb.co/DG69bQ4/2.png" />
+          </ImageContainer>
+          <InfoContainer>
+            <Title>AUTUMN COLLECTION</Title>
+            <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Dignissimos harum atque sequi molestias debitis eos mollitia 
+              dolorem nesciunt aut quod dolore vel, ullam voluptate. 
+              Totam voluptas distinctio error corrupti sunt?</Desc>
+              <Price>$ 20</Price>
+              <FilterContainer>
+                <Filter>
+                  <FilterTitle>Color</FilterTitle>
+                  <FilterColor color='black'/>
+                  <FilterColor color='darkblue'/>
+                  <FilterColor color='gray'/>
+                </Filter>
+                <Filter>
+                  <FilterTitle>Size</FilterTitle>
+                  <FilterSize>
+                    <FilterSizeOption>XS</FilterSizeOption>
+                    <FilterSizeOption>S</FilterSizeOption>
+                    <FilterSizeOption>M</FilterSizeOption>
+                    <FilterSizeOption>L</FilterSizeOption>
+                    <FilterSizeOption>XL</FilterSizeOption>
+                  </FilterSize>
+                </Filter>
+              </FilterContainer>
+              <AddContainer>
+                <AmmountContainer>
+                  <RemoveIcon/>
+                  <Ammount>1</Ammount>
+                  <AddIcon/>
+                </AmmountContainer>
+                <Button>ADD TO CART</Button>
+              </AddContainer>
+          </InfoContainer>
+        </Wrapper>
+      <NewsLetter/>
+      <Footer/>
+    </Container>
   )
 }
 
