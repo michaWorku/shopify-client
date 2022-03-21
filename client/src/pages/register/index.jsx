8d5dcf29ea879e77544670ce1efc93b950fcd867
michaworku <mikias27worku@gmail.com>
2022-03-21 14:04:49 +0300

    register page design

--- client/src/pages/register/index.jsx
+++ client/src/pages/register/index.jsx
@@ -1,6 +1,25 @@
+import { Agreement, Button, Container, Title, Form, Input, Wrapper } from "./styled"
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <Container>
+      <Wrapper>
+      <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder='name'></Input>
+          <Input placeholder='last name'></Input>
+          <Input placeholder='username'></Input>
+          <Input placeholder='email'></Input>
+          <Input placeholder='password'></Input>
+          <Input placeholder='confirm password'></Input>
+          <Agreement>
+            By creating an account, I consent to the processing of My personal
+            data in accordance with <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
   )
 }
 
---
+++ client/src/pages/register/styled.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background:
+        linear-gradient(
+            rgba(255, 255, 255, 0.2),
+            rgba(255, 255, 255, 0.2)
+        ),
+        url("https://media.istockphoto.com/photos/online-shopping-concept-laptop-and-shopping-cart-on-blue-background-picture-id1301022916?b=1&k=20&m=1301022916&s=170667a&w=0&h=SZwrubcWrhzWgomr41shkT15TzZyjta9ubUoFPbmEiM=")
+        center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `
+
+export const Wrapper = styled.div`
+    width: 40%;
+    padding: 60px;
+    margin-left: 400px;
+    background-color: white;
+`
+
+export const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+export const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`
+
+export const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0px;
+`
+
+export const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`
