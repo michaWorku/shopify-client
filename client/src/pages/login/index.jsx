3d9164f0374cc133252f7c6aac2fe1562f802376
michaworku <mikias27worku@gmail.com>
2022-03-24 12:23:57 +0300

    login page

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,13 +1,82 @@
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
+import { Spinner } from "../../components"
+import { login, reset } from "../../features/auth/authSlice"
+
 import { Button, Container, Title, Form, Input, Wrapper, Link } from "./styled"
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+  
   return (
     <Container>
       <Wrapper>
       <Title>SIGN IN</Title>
-        <Form>
-          <Input placeholder='username'></Input>
-          <Input placeholder='password'></Input>
+        <Form onSubmit={onSubmit}>
+          <Input 
+            type='email'
+            id='email'
+            name='email'
+            value={email}
+            placeholder='Enter your email'
+            onChange={onChange}>
+          </Input>
+          <Input 
+            type='password'
+            id='password'
+            name='password'
+            value={password}
+            placeholder='Enter password'
+            onChange={onChange}
+          ></Input>
           <Button>LOGIN</Button>
           <Link>DON NOT YOU REMEMBER THE PASSWORD?</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
